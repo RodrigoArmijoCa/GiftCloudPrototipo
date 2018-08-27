c9b5a275043300814f99ac35f372e84fea7fb407
KnightBloodZero <tenchu-_-@hotmail.com>
2018-08-27 15:45:40 -0300

    Barrita de saldo en la tienda.

--- app/src/main/java/com/example/yoyi_pc/giftcloud/TiendaActivity.java
+++ app/src/main/java/com/example/yoyi_pc/giftcloud/TiendaActivity.java
@@ -3,19 +3,32 @@ package com.example.yoyi_pc.giftcloud;
 import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class TiendaActivity extends AppCompatActivity implements TiendaFragment.OnFragmentInteractionListener{
 
     static ArrayList<String> listaDeNomPesta = new ArrayList<String>();
+    static public ActionBar barritaSuperior;
+    static public View vista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tienda);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar2);
+        barritaSuperior = getSupportActionBar();
+        vista = getSupportActionBar().getCustomView();
+        TextView mayo = vista.findViewById(R.id.valorGiftCoins2);
+        TextView otro = MainActivity.vista.findViewById(R.id.valorGiftCoins);
+        mayo.setText(otro.getText());
         iniciaNombrePesta();
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayoutTienda);
         configTabs(tabLayout, listaDeNomPesta.size(), listaDeNomPesta);
